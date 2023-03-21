b7c07ead452730de89cea460531f7cd05157bcc7
Ebad Sayed <95970293+ES7@users.noreply.github.com>
2023-03-21 15:32:52 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hw():
-  return "hello world"
+  return render_template("home.html")
 
 if __name__ == "__main__":
   aap.run(host="0.0.0.0", debug=True)
